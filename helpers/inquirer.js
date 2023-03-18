dd6d498d9727f8da6b33fe0cd77a6fdbf0f0e28c
Angel Quispe <angelquispe_2997@outlook.com>
2023-03-18 07:12:34 -0500

    Proyecto avanzado hasta consulta del lugar con MAPBOX API

---
+++ helpers/inquirer.js
@@ -0,0 +1,158 @@
+const inquirer = require('inquirer');
+require('colors');
+
+const choices = [
+  {
+    value: 1,
+    name: `${'1.'.green} Buscar ciudad`
+  },
+  {
+    value: 2,
+    name: `${'2.'.green} Historial`
+  },
+  {
+    value: 0,
+    name: `${'0.'.green} Salir`
+  }
+]
+
+const preguntas = [
+  {
+    type: 'list',
+    name: 'opcion',
+    message: 'Qué desea hacer',
+    choices,
+  }
+];
+
+const inquirerMenu = async() => {
+  
+  // console.clear();
+  console.log('='.green.repeat(25));
+  console.log('  Seleccione una opción'.white);
+  console.log('='.green.repeat(25),'\n');
+
+  const {opcion} = await inquirer.prompt(preguntas);
+
+  return opcion;
+
+}
+
+const pausa = async() => {
+
+  const question = [{
+    type: 'input',
+    name: 'enter',
+    message: `Presione ${'ENTER'.green} para continuar`
+  }]
+   
+  console.log('\n');
+  await inquirer.prompt(question);
+
+}
+
+const leerInput = async( message ) => {
+
+  const question = [
+    {
+      type: 'input',
+      name: 'desc',
+      message,
+      validate(value) {
+        if (value.length === 0) {
+          return 'Por favor ingrese un valor'
+        }
+        return true;
+      }
+    }
+  ];
+
+  const {desc} = await inquirer.prompt(question);
+
+  return desc;
+}
+
+const listarLugares = async (lugares = []) => {
+
+  const choices = lugares.map((lugar,i) => {
+
+    const idx = `${i+1}.`.green;
+
+    return {
+      value: lugar.id,
+      name: `${idx} ${lugar.nombre}`
+    }
+  });
+
+  choices.unshift({
+    value: '0',
+    name: '0.'.green + ' Cancelar'
+  });
+
+  const preguntas = [
+    {
+      type: 'list',
+      name: 'id',
+      message: 'Seleccione lugar:',
+      choices
+    }
+  ]
+
+  const {id} = await inquirer.prompt(preguntas);
+
+  return id;
+
+}
+
+const confirm = async (message) => {
+
+  const question = [
+    {
+      type: 'confirm',
+      name: 'ok',
+      message
+    }
+  ];
+
+  const {ok} = await inquirer.prompt(question);
+  return ok;
+
+}
+
+const showListChecklist = async (tasks) => {
+
+  const choices = tasks.map((task,i) => {
+
+    const idx = `${i+1}.`.green;
+
+    return {
+      value: task.id,
+      name: `${idx} ${task.desc}`,
+      checked: (task.completadoEn) ? true : false
+    }
+  });
+
+  const pregunta = [
+    {
+      type: 'checkbox',
+      name: 'ids',
+      message: 'Seleccione',
+      choices
+    }
+  ]
+
+  const {ids} = await inquirer.prompt(pregunta);
+
+  return ids;
+
+}
+
+
+module.exports = {
+  inquirerMenu,
+  pausa,
+  leerInput,
+  listarLugares,
+  confirm,
+  showListChecklist
+}
---
+++ index.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+
+const { inquirerMenu, pausa, leerInput, listarLugares } = require("./helpers/inquirer");
+const Busquedas = require("./models/busqueda");
+
+
+
+const main = async () => {
+
+  const busquedas = new Busquedas();
+  let opt;
+
+  do {
+    
+    opt = await inquirerMenu();
+    
+    switch (opt) {
+      case 1:
+        // Mostrar mensaje
+        const termino = await leerInput('Ciudad: ');
+        
+        // Buscar los lugares
+        const lugares = await busquedas.ciudad( termino );
+        
+        // Seleccionar el lugar
+        const id = await listarLugares(lugares);
+        const lugarSel = lugares.find(l => l.id === id);
+
+        // Clima
+
+        // Mostrar resultados
+        console.log('\nInformación de la ciudad\n'.green);
+        console.log('Ciudad: ', lugarSel.nombre);
+        console.log('Lat: ', lugarSel.lat);
+        console.log('Lon: ', lugarSel.lng);
+        console.log('Temperatura: ',);
+        console.log('Mínima: ',);
+        console.log('Maxima: ',);
+        break;
+    
+      default:
+        break;
+    }
+    
+    if(opt !== 0) {
+      await pausa();
+    }
+
+
+
+  } while (opt !== 0);
+
+}
+
+main();
